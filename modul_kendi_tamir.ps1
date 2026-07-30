58cfd740cf228562a70a202b3cee7d7e8f0ec2ab
CodeAnd-Art <3838yos4141@gmail.com>
2026-07-30 11:33:43 +0300

    Update modul_kendi_tamir.ps1

--- modul_kendi_tamir.ps1
+++ modul_kendi_tamir.ps1
@@ -1,11 +1,15 @@
 # ============================================================
-# EN – KENDİ KENDİNE TAMİR (SELF-HEALING)
+# EN – KENDİ KENDİNE TAMİR (GÜNCELLENDİ)
 # ============================================================
 
 $gerekliDosyalar = @(
     "EN.bat",
+    "config.txt",
+    "EN.png",
     "modul_ortam_tespit.ps1",
+    "modul_guvenlik_duvari.ps1",
     "modul_sinir_seviyesi.ps1",
+    "modul_sinir_sesi.ps1",
     "modul_chat.ps1",
     "modul_kernel_ye.ps1",
     "modul_kufur_listesi.ps1",
@@ -13,7 +17,6 @@ $gerekliDosyalar = @(
     "modul_kaynak_doldur.ps1",
     "modul_uyari_ana_cihaz.ps1",
     "modul_vm_ayar_oyna.ps1",
-    "modul_guvenlik_duvari.ps1",
     "modul_ekran_bozma.ps1",
     "modul_sistem_dosyasi_ye.ps1",
     "modul_mesaj_havuzu.ps1",
@@ -21,7 +24,20 @@ $gerekliDosyalar = @(
     "modul_sacma_olay.ps1",
     "modul_yonetici_yetkisi.ps1",
     "modul_son_ekran.ps1",
-    "modul_baslangic_ayarlari.ps1"
+    "modul_baslangic_ayarlari.ps1",
+    "modul_element_sorusu.ps1",
+    "modul_mini_oyun.ps1",
+    "modul_virus_sorusu.ps1",
+    "modul_sistem_dosyasi_sorusu.ps1",
+    "modul_video_ac.ps1",
+    "modul_video_kapatma.ps1",
+    "modul_kustum.ps1",
+    "modul_cok_agir_hakaret.ps1",
+    "modul_dil_destek.ps1",
+    "modul_log.ps1",
+    "modul_pislik_cezasi.ps1",
+    "modul_config_oku.ps1",
+    "modul_ses.ps1"
 )
 
 $tamirLog = "C:\EN_Tamir_Log.txt"
